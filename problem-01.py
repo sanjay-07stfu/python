c5e4bddd42b14e2aa731a46855b1efa6628b6943
sanjay <yedagesanjay8@gmail.com>
2025-11-22 19:28:31 +0530

    sanjay

--- problem-01.py
+++ problem-01.py
@@ -1,3 +1,10 @@
-#7. Find the second largest number in a list
-
+# Find the second largest number in a list
 #Example: [10, 20, 4, 45, 99] → 45
+
+list=[10, 20, 4, 45, 99]
+
+for i in range(len(list)):
+    if (i>=99 and i<20):
+      print(list[i])
+      
+     
