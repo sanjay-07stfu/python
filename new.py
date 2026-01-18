7d67336deff9cab9bd93dec13754a0067a935072
sanjay <yedagesanjay8@gmail.com>
2026-01-18 20:29:08 +0530

    new

--- new.py
+++ new.py
@@ -7,3 +7,23 @@
 
 
 # print("sanjay yedage")
+
+# array=[10,20,30,40,50]
+
+# for x in array:
+#     print(x)
+
+
+
+
+from flask import Flask
+from datetime import date
+
+app = Flask(__name__)
+
+@app.route('/')
+def show_date():
+    return f"<h2>Today's date is: {date.today()}</h2>"
+
+if __name__ == '__main__':
+    app.run()
