44d2439d0b0aeb5d9c888398c3fd7686bb7fb693
sanjay <yedagesanjay8@gmail.com>
2026-06-01 18:35:21 +0530

    newwq

--- area .py
+++ area .py
@@ -19,3 +19,8 @@ base=int(input("Enter the base of triangle:"))
 height=int(input("Enter the height of triangle:"))
 area=0.5*base*height
 print("Area of triangle is:",area)\
+
+
+
+
+
---
+++ practice.py
@@ -0,0 +1,6 @@
+# num=20
+# num -=10 
+# print (num)
+
+a=23
+print (type(a))
